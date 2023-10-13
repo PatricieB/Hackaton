633194df83d736ddd6cd2098a5b1f0b54b9357d2
pabenesova <patricie.benesova@generaliceska.cz>
2023-10-13 09:45:10 +0200

    pokusy

--- main.py
+++ main.py
@@ -2,23 +2,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import imageio.v3 as iio
+import cv2
 # from keras.datasets import mnist
 
+from skimage.filters import threshold_otsu
+from skimage import exposure
 
-data = iio.imread('train_set/solar/AABCN78E1YHCWW.png')
 
 
-# Rozměry načtených obrázků
-print(f"Shape of test images: {data.shape}")
-print(f"Shape of an individual image: {data[0].shape}")
+originalImage = cv2.imread('train_set/solar/AABCN78E1YHCWW.png')
+grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
 
-# Zobrazení prvního příkladu ve formě obrázku
-plt.imshow(data, cmap='gray')
-#plt.title(f"Label: {test_labels[0]}")
+# Histogram equalization na prvním příkladu
+#equalized_image = exposure.equalize_hist(data)
+
+ada_thresh_gaussian = cv2.adaptiveThreshold(grayImage, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 4)
+
+# Zobrazení před a po
+
+plt.subplot(1, 2, 2)
+plt.imshow(ada_thresh_gaussian, cmap='gray')
+plt.title("Equalized Image")
 plt.axis("off")
+
 plt.show()
 
-# Tisk prvního příkladu ve formě matice
-print(f"Matrix representation of the above image:\n")
-for row in data[0]:
-    print(' '.join([f'{num:3}' for num in row]))
+
+
+
+
+
